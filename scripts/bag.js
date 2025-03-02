01aef6409b479d90c10add83cbb1fbe547f9fe6a
samarthbiraje26 <samarthbiraje@gmail.com>
2025-03-02 21:20:21 +0530

    Update bag.js
    made it more generic

--- scripts/bag.js
+++ scripts/bag.js
@@ -20,31 +20,31 @@ function loadBagItemObjects() {
 
 function displayBagItems() {
     let containerElement = document.querySelector('.bag-items-container');
-    containerElement.innerHTML = `<div class="bag-item-container">
+    //containerElement.innerHTML = ;
+}
+
+function generateItemHTML(item) {
+    return `<div class="bag-item-container">
             <div class="item-left-part">
-              <img class="bag-item-img" src="../images/4.jpg">
+              <img class="bag-item-img" src="../${item.image}">
             </div>
             <div class="item-right-part">
-              <div class="company">ADIDAS</div>
-              <div class="item-name">Men Printed Polo Collar Indian Cricket ODI Jersey</div>
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
               <div class="price-container">
-                <span class="current-price">Rs 999</span>
-                <span class="original-price">Rs 999</span>
-                <span class="discount-percentage">(0% OFF)</span>
+                <span class="current-price">Rs ${item.current_price}</span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount-percentage">(${discount_percentage}% OFF)</span>
               </div>
               <div class="return-period">
-                <span class="return-period-days">14 days</span> return available
+                <span class="return-period-days">${item.return_period} days</span> return available
               </div>
               <div class="delivery-details">
                 Delivery by
-                <span class="delivery-details-days">10 Oct 2023</span>
+                <span class="delivery-details-days">${item.delivery_date}</span>
               </div>
             </div>
 
             <div class="remove-from-cart">X</div>
-          </div>`
-}
-
-function generateItemHTML(item) {
-
+          </div>`; 
 }
