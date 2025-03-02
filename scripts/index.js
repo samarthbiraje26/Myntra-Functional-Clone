8a53228e37a524448ceb0cafd034397109938c40
samarthbiraje26 <samarthbiraje@gmail.com>
2025-03-02 20:18:10 +0530

    Update index.js
    made the bag full functional and updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,27 @@
-let bagItems = [];
-displayItemsOnHomePage();
+let bagItems;
+onLoad();
+
+function onLoad() {
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+    displayItemsOnHomePage();
+    displayBagIcon();
+}
 
 function addToBag(itemId) {
     bagItems.push(itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    displayBagIcon();
+}
+
+function displayBagIcon () {
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (bagItems.length > 0) {
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerText = bagItems.length;
+    } else {
+    bagItemCountElement.style.visibility = 'hidden';
+    }
 }
 
 function displayItemsOnHomePage () {
